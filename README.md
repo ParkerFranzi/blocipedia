8f6176f44ad67a8f16c49349db13a12c9d5bb594
Parker <parker.franzi@gmail.com>
2013-12-19 18:51:17 -0800

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blocipedia
+
+A Wiki style application to work on my programming and web development skills
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = '9257e04f8cc56cc951ebc24dc238dfa04cd8bb15a0b23a03c21d5f0b3d9e8297b84f5e874f8663957a700afa9dfded4aceaf26724c9abce70d2820ab65ff6a93'
